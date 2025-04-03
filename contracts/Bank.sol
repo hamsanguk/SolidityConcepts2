07fcd14a839b8c0ccce53377b2345d63218f0e3a
함상욱 <hamsang-ug@hamsang-ug-ui-MacBookPro-2.local>
2025-04-03 17:57:13 +0900

    day42

--- contracts/Bank.sol
+++ contracts/Bank.sol
@@ -1,1 +1,26 @@
-contract Bank {}
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./Vault.sol";
+
+contract Bank is Vault {
+    event Withdrawn (address indexed user, uint amount);
+    
+    modifier onlyOwner(){
+        require(msg.sender == owner,'Only the owner can call withdraw.');
+        _;
+    }
+
+    function withdraw (uint amount) public onlyOwner{
+        require(amount <= sentValue, "Insufficient balance in Vault.");
+        sentValue -= amount;
+        payable(msg.sender).transfer(amount);
+        emit Withdrawn(msg.sender, amount);
+    }
+    // function withdraw(uint amount) public onlyOwner{
+    //     require(amount <= sentValue, "Insufficient balance in Vault.");
+    //     sentValue -= amount;
+    //     payable(msg.sender).transfer(amount);
+    //     emit Withdrawn(msg.sender, amount);
+    // }
+}
--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -1,1 +1,36 @@
-contract Vault {}
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+contract Vault {
+
+    address public owner;
+    uint public sentValue;
+    uint public timestamp;
+    uint public gasUsed;
+
+    constructor() {
+        owner = msg.sender;
+    }
+    function deposit () public virtual payable {
+        if (msg.value <= 0) {
+            revert ("Must send some ether.");
+        }
+        sentValue = msg.value;
+        timestamp = block.timestamp;
+    }
+    function getCaller() public view returns (address) {
+        return msg.sender;
+    }
+    function getOrigin() public view returns (address) {
+        return tx.origin;
+    }
+    function getBlockDetails() public view returns (uint, uint, uint, address) {
+        return (block.number, block.prevrandao, block.gaslimit, block.coinbase);
+    }
+    function trackGasUsage() public {
+        gasUsed =  tx.gasprice;
+    }
+    function generateHash(string memory input) public pure returns (bytes32){
+        return keccak256(abi.encodePacked(input));
+    }
+    
+}
